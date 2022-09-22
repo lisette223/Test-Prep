dae57e6bcf08d67696e091358a3394116ee0ceab
LisetteAntigua <antigua223@gmail.com>
2022-09-22 11:50:53 -0400

    aaa

---
+++ TestReview.playground/Contents.swift
@@ -0,0 +1,99 @@
+import Foundation
+/*: ## DO NOW
+ 
+Create a function that takes two integers and checks if they are equal. If they are equal, print "These numbers are the same". If the first integer is larger, print "This number is larger". Else, print the second number is larger.*/
+
+ 
+
+ 
+/*: ## Test Review
+ 
+1) Write a function named isNegative. It accepts two integer values and return true if one is negative and one is positive. Return true only if both are negative.
+*/
+ 
+
+
+/*:
+ 2) Write a function that adds "Is" to the front of a given string. However, if the string already begins with "Is", return the given string.
+ */
+ 
+
+
+
+/*:
+3) We’ve set up an if/else statement that checks for windiness. On the following line, rewrite the conditional in ternary format.
+*/
+var windy = true
+
+if windy {
+  print("Sails up")
+} else {
+  print("Motor on")
+}
+
+
+
+/*:
+4) We have a series of else if statements that match a superhero’s secret identity to their superhero name. Rewrite this conditional in switch statement format.
+*/
+
+var secretIdentity = "Tony Stark"
+var superheroName: String
+ 
+if secretIdentity == "Tony Stark" {
+  superheroName = "Iron Man"
+} else if secretIdentity == "Natasha Romanoff" {
+  superheroName = "Black Widow"
+} else if secretIdentity == "Prince T'Challa" {
+  superheroName = "Black Panther"
+} else if secretIdentity == "Thor" {
+  superheroName = "Thor"
+} else {
+  superheroName = "Unknown"
+}
+
+
+
+
+/*:
+5) Create a function that takes two numbers as arguments and return their sum.
+Examples
+addition(3, 2) ➞ 5
+addition(-3, -6) ➞ -9
+addition(7, 3) ➞ 10
+*/
+
+
+
+
+
+/*: 6) Write a function named min2 that takes two Int values, a and b, and returns the smallest one. Use _ to ignore the external parameter names for both a and b. */
+
+
+
+
+
+
+
+/*:
+7) Write a function that takes an Int and returns it’s last digit. Name the function lastDigit. Use _ to ignore the external parameter name.
+
+*/
+
+
+
+
+
+
+
+
+
+/*: ## Exit Ticket
+
+Write a function that takes two integers (hours, minutes), converts them to seconds, and adds them.
+
+ 
+ */
+
+
+
